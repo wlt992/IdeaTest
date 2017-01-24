9cfac7e70bd65d736d757d7fb3f11dc4d9c2452d
ltwu <mail address>
2017-01-24 18:48:23 +0800

    test git

--- src/com/ctrip/wlt/HelloWorld.java
+++ src/com/ctrip/wlt/HelloWorld.java
@@ -6,5 +6,6 @@ package com.ctrip.wlt;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("hello world!");
+        System.out.println("test git!");
     }
 }
