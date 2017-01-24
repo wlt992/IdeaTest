fe41064adc45f6ec20a9d4a94205a9c44697b75c
ltwu <mail address>
2017-01-24 18:42:18 +0800

    Initial commit

---
+++ src/com/ctrip/wlt/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ctrip.wlt;
+
+/**
+ * Created by ltwu on 2017/1/24.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
+}
